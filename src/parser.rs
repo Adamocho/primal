577b08ae6398a28568805f019bcaea269c3b8104
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-07-14 18:21:05 +0200

    Progress further with parser: - implement every function - complete match trees of recognize_statement - clean some ugly 5-tabs code

--- src/parser.rs
+++ src/parser.rs
@@ -1,21 +1,19 @@
 use core::panic;
-use std::{any::Any, process::exit};
 
 use crate::lexer::Token;
-
-// THINGS:
-// bubble up the syntax-error messages if possible
-// dont know the type of `tree` yet
+use crate::lexer::{BOOL_ID, STRING_ID, NUMBER_ID, IDENTIFIER_ID};
 
 #[derive(Debug)]
 pub struct Parser {
     tokens: Vec<Token>,
     start: usize,
     counter: usize,
-    program: Vec<Token>, // dont know
+    program: Vec<Token>,
     current: Option<Token>,
     next: Option<Token>,
 }
+
+const PLACEHOLDER: String = String::new();
 
 impl Parser {
     pub fn new(tokens: Vec<Token>) -> Parser {
@@ -49,61 +47,229 @@ impl Parser {
             // "PRINT" string nl
             Some(Token::Print) => {
                 println!("Print-statement");
-
                 self.next_token();
-                self.match_token(Token::String("".to_string()));
+
+                self.match_token(Token::String(PLACEHOLDER, STRING_ID));
 
                 self.newline();
-
-                dbg!(&self);
-                todo!("IMPLEMENT THIS FURTHER");
             },
+            // "LET" identifier "=" expression nl
             Some(Token::Let) => {
                 println!("Let-statement");
+                self.next_token();
+
+                self.match_token(Token::Identifier(PLACEHOLDER, IDENTIFIER_ID));
+
+                self.match_token(Token::Assign);
+
+                self.expression();
+
+                self.newline();
             },
+            // "IF" comparison "THEN" nl {statement} nl "ENDIF" nl
             Some(Token::If) => {
                 println!("If-statement");
+                self.next_token();
+
+                self.comparisons();
+                self.match_token(Token::Then);
+                self.newline();
+
+                // ZERO OR MORE???
+                self.recognize_statement();
+
+                // ?? 
+                self.newline();
+
+                self.match_token(Token::Endif);
+                self.newline();
             },
+            // "WHILE" comparison nl "DO" {statement} nl "ENDWHILE" nl
             Some(Token::While) => {
                 println!("While-statement");
+                self.next_token();
+
+                self.comparisons();
+                self.newline();
+                self.match_token(Token::Do);
+
+                // ZERO OR MORE???
+                self.recognize_statement();
+
+                // ?? 
+                self.newline();
+
+                self.match_token(Token::Endwhile);
+                self.newline();
             },
+            // "INPUT" string identifier nl
             Some(Token::Input) => {
                 println!("Input-statement");
+                self.next_token();
+
+                self.match_token(Token::String(PLACEHOLDER, STRING_ID));
+                self.match_token(Token::Identifier(PLACEHOLDER, IDENTIFIER_ID));
+                self.newline();
             },
+            // "GOTO" identifier nl
             Some(Token::Goto) => {
                 println!("Goto-statement");
+                self.next_token();
+
+                self.match_token(Token::Identifier(PLACEHOLDER, IDENTIFIER_ID));
+                self.newline();
             },
             Some(Token::Newline) => {
                 println!("Newline");
+
+                // clear newlines till next different token
+                self.newline();
             },
             _ => {
-                println!("Syntax error: Token not recognized: {:#?}", self.current);
-                exit(1);
+                panic!("Syntax error: Token not recognized: {:#?}", self.current);
             }
         }
-        println!("Passed")
     }
 
-    // match current
-    fn match_token(&mut self, expected: Token) {
-        match expected {
-            Token::Bool(_) | Token::String(_) | Token::Number(_) | Token::Identifier(_) => {
-                println!("Not checked until you find a better solution...");
+    // comparisons ::= comparison {("AND" | "OR") comparison}
+    fn comparisons(&mut self) {
+        println!("comparisons - plural");
+
+        self.comparison();
+
+        while self.current == Some(Token::And) || self.current == Some(Token::Or) {
+            self.next_token();
+
+            self.comparison();
+        } 
+    }
+
+    // comparison ::= (expression equals expression) | ("true" | "false")
+    fn comparison(&mut self) {
+        println!("Checking a comparison");
+
+        match self.current {
+            Some(Token::Bool(_, _)) => {
+                self.next_token();
+                return;
+            }
+            _ => {}
+        }
+
+        self.expression();
+
+        self.equals();
+
+        self.expression();
+    }
+
+    // expression ::= term {("+" | "-") term}
+    fn expression(&mut self) {
+        println!("Evaluating expression");
+
+        self.term();
+
+         while self.current == Some(Token::Plus) || self.current == Some(Token::Minus) {
+            self.next_token();
+
+            self.term();
+        }
+
+    }
+
+    // term ::= unary {("*" | "/" | "%") unary}
+    fn term(&mut self) {
+        println!("Term");
+
+        self.unary();
+
+         while self.current == Some(Token::Times)
+        || self.current == Some(Token::Divide)
+        || self.current == Some(Token::Modulo) {
+            self.next_token();
+
+            self.unary();
+        }
+    }
+
+    // unary ::= ["+" | "-"] primary
+    fn unary(&mut self) {
+        println!("Unary");
+
+        if self.current == Some(Token::Plus) || self.current == Some(Token::Minus) {
+            self.next_token();
+        }
+
+        self.primary();
+    }
+
+    // primary ::= identifier | number
+    fn primary(&mut self) {
+        println!("Primary");
+
+        match self.current {
+            Some(Token::Identifier(_, id)) | Some(Token::Number(_, id)) => {
+                if id == IDENTIFIER_ID || id == NUMBER_ID { self.next_token(); return; }
+            },
+            _ => {}
+        }
+
+        Self::abort(
+            format!(
+                "expected {:#?} OR {:#?}, got {:#?}",
+                Token::Identifier(PLACEHOLDER, IDENTIFIER_ID),
+                Token::Number(0, NUMBER_ID),
+                self.current
+            )
+        );
+    }
+
+    // complex ::= identifier | string
+    fn complex(&mut self) {
+        println!("Complex");
+
+        match self.current {
+            Some(Token::Identifier(_, id)) | Some(Token::String(_, id)) => {
+                if id == IDENTIFIER_ID || id == STRING_ID { self.next_token(); return; }
+            },
+            _ => {}
+        }
+
+        Self::abort(
+            format!(
+                "expected {:#?} OR {:#?}, got {:#?}",
+                Token::Identifier(PLACEHOLDER, IDENTIFIER_ID),
+                Token::String(PLACEHOLDER, STRING_ID),
+                self.current
+            )
+        );
+    }
+
+
+    // equals ::= ("==" | "!=" | "<=" | ">=" | ">" | "<")
+    fn equals(&mut self) {
+        match self.current {
+            Some(Token::Equals) | Some(Token::NotEquals) |
+            Some(Token::MoreThan) | Some(Token::MoreThanEquals) |
+            Some(Token::LessThan) | Some(Token::LessThanEquals) => {
+                self.next_token();
             }
             _ => {
-                if Some(&expected) != self.current.as_ref() {
-                    Self::abort(format!("expected {:#?}, got {:#?}", expected, self.current));
-                }
+                Self::abort(
+                    format!(
+                        "expected one of: {:#?}, {:#?}, {:#?}, {:#?}, {:#?} or {:#?}, got {:#?} instead",
+                        Some(Token::Equals),
+                        Some(Token::NotEquals),
+                        Some(Token::MoreThan),
+                        Some(Token::MoreThanEquals),
+                        Some(Token::LessThan),
+                        Some(Token::LessThanEquals),
+                        self.current
+                ));
             }
         }
-
-        self.next_token()
-    }
-
-    fn next_token(&mut self) {
-        self.current = self.next.clone();
-        self.next = self.tokens.get(self.counter).cloned();
-        self.counter += 1;
+        
+        self.next_token();
     }
 
     // nl ::= '\n'+
@@ -117,9 +283,42 @@ impl Parser {
         }
     }
 
+    // match current
+    fn match_token(&mut self, expected: Token) {
+        match expected {
+            Token::Bool(_, id) | Token::String(_, id) | Token::Number(_, id) | Token::Identifier(_, id) => {
+                self.match_value_token(expected, id);
+            }
+
+            _ => {
+                if Some(&expected) != self.current.as_ref() {
+                    Self::abort(format!("expected {:#?}, got {:#?}", expected, self.current));
+                }
+            }
+        }
+        self.next_token();
+    }
+
+    // match special cases of tokens - Bool, String, Number, Identifier
+    fn match_value_token(&mut self, expected: Token, id: u8) {
+        match self.current {
+            Some(Token::Bool(_, current_id)) | Some(Token::String(_, current_id)) |
+            Some(Token::Number(_, current_id)) | Some(Token::Identifier(_, current_id)) => {
+                if id == current_id { return; }
+            }
+            _ => {}
+        }
+        Self::abort(format!("expected {:#?}, got {:#?}", expected, self.current));
+    }
+
+    fn next_token(&mut self) {
+        self.current = self.next.clone();
+        self.next = self.tokens.get(self.counter).cloned();
+        self.counter += 1;
+    }
+
     fn abort(message: String) {
-        println!("Syntax error: {}", message);
-        exit(1);
+        panic!("Syntax error: {}", message);
     }
 }
 
